f5e8e99a1dff4d86f20db41bedc9b16d7855bcff
Lucas Schwendler Vieira <lucas.dsv@hotmail.com>
2020-06-20 14:46:29 -0300

    Iniciar projeto com Spring Initializr

---
+++ src/main/java/com/hotelmanagement/guestassured/GuestassuredApplication.java
@@ -0,0 +1,13 @@
+package com.hotelmanagement.guestassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestassuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestassuredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelmanagement/guestassured/GuestassuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelmanagement.guestassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestassuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
