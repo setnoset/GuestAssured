31092894e560d86c2d64d727a79d536c457f30aa
Lucas Schwendler Vieira <lucas.dsv@hotmail.com>
2020-06-21 02:43:26 -0300

    Criar interface GuestRepository para gerenciar hospedes em armazenamento persistente

---
+++ src/main/java/com/hotelmanagement/guestassured/GuestRepository.java
@@ -0,0 +1,6 @@
+package com.hotelmanagement.guestassured;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface GuestRepository extends PagingAndSortingRepository<Guest, Long> {
+}
