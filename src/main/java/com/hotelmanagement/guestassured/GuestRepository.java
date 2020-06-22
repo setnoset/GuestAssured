d83be9c06cd2e761831d45b76b64f5e033d3d3b5
Lucas Schwendler Vieira <lucas.dsv@hotmail.com>
2020-06-22 06:58:27 -0300

    Renomear tabela checkin para a correta, check_in, dentro dos SQL

--- src/main/java/com/hotelmanagement/guestassured/GuestRepository.java
+++ src/main/java/com/hotelmanagement/guestassured/GuestRepository.java
@@ -17,10 +17,10 @@ public interface GuestRepository extends PagingAndSortingRepository<Guest, Long>
     @Query(value = "SELECT * FROM guests ORDER BY id LIMIT 10 OFFSET 10*:pageid")
     List<Guest> findAll(int pageid);
 
-    @Query(value = "SELECT * FROM guests WHERE id IN (SELECT guest FROM checkin WHERE date_in < now() AND now() < date_out) ORDER BY id LIMIT 10 OFFSET 10*:pageid")
+    @Query(value = "SELECT * FROM guests WHERE id IN (SELECT guest FROM check_in WHERE date_in < now() AND now() < date_out) ORDER BY id LIMIT 10 OFFSET 10*:pageid")
     List<Guest> findAllInHotel(int pageid);
 
-    @Query(value = "SELECT * FROM guests WHERE id NOT IN (SELECT guest FROM checkin WHERE date_in < now() AND now() < date_out) ORDER BY id LIMIT 10 OFFSET 10*:pageid")
+    @Query(value = "SELECT * FROM guests WHERE id NOT IN (SELECT guest FROM check_in WHERE date_in < now() AND now() < date_out) ORDER BY id LIMIT 10 OFFSET 10*:pageid")
     List<Guest> findAllNotInHotel(int pageid);
 
 }
