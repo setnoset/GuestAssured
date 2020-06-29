f0ccbdf3ef17a947f131fb54d8cc73b4e93f9413
Lucas Schwendler Vieira <lucas.dsv@hotmail.com>
2020-06-29 07:01:00 -0300

    Adicionar testes para pegar os dados de um hospede por id atravez de GET /guests/{id}

--- src/test/java/com/hotelmanagement/guestassured/GuestControllerTest.java
+++ src/test/java/com/hotelmanagement/guestassured/GuestControllerTest.java
@@ -6,8 +6,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.annotation.Id;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -51,5 +53,35 @@ public class GuestControllerTest {
         when(guestRepositoryMock.save(guest)).thenThrow(new DataIntegrityViolationException("Attempting to insert null value into non null column"));
 
         assertThrows(ResponseStatusException.class, () -> guestController.newGuest(guest));
+    }
+
+    @Test
+    public void testGetExistingGuest() {
+        Guest guest = new Guest(7L, "Fulano Teste 2", "297884743", "5541948226148");
+        LocalDateTime dateIn = LocalDateTime.of(2020, 4, 10, 17, 0);
+        LocalDateTime dateOut = LocalDateTime.of(2020, 4, 18, 12, 0);
+        CheckIn lastCheckIn = new CheckIn(3L, 7L, dateIn, dateOut, true);
+
+        when(guestRepositoryMock.findById(7L)).thenReturn(Optional.of(guest));
+        when(checkInRepositoryMock.countByGuest(7L)).thenReturn(3L);
+        when(checkInRepositoryMock.findFirstByGuestOrderByDateOut(7L)).thenReturn(lastCheckIn);
+        when(checkInRepositoryMock.guestExpenditure(7L)).thenReturn(Optional.of(4000.0f));
+
+        GuestDTO guestDTO = guestController.getGuest(7L);
+
+        assertEquals(7L, guestDTO.getId());
+        assertEquals(guest.getName(), guestDTO.getName());
+        assertEquals(guest.getDocument(), guestDTO.getDocument());
+        assertEquals(guest.getPhone(), guestDTO.getPhone());
+        assertEquals(3L, guestDTO.getCheckInCount());
+        assertEquals(1450.0f, guestDTO.getLastCheckInCost());
+        assertEquals(4000.0f, guestDTO.getTotalSpent());
+    }
+
+    @Test
+    public void testNonExistingGuest() {
+        when(guestRepositoryMock.findById(7L)).thenReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> guestController.getGuest(7L));
     }
 }
